dccf8edf4eebe69681bb83f2ca1fd5ef3a25f068
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-03-06 09:34:39 -0800

    4 components done

--- src/App.js
+++ src/App.js
@@ -2,20 +2,9 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Total from './components/Total';
 import Cart from './components/Cart';
-import SelectProcessor from './components/SelectProcessor';
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
-
+import Features from './components/Features';
 import './App.css';
 
-// This object will allow us to
-// easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
 
 class App extends Component {
   constructor(props) {
@@ -41,9 +30,7 @@ class App extends Component {
         }
       }
     };
-  }
-
-  
+  }  
 
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
@@ -53,46 +40,14 @@ class App extends Component {
     });
   };
 
-  render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-  
-
+  render() { 
     return (
       <div className="App">
         <Header />
         <main>
           <form className="main__form">
             <h2>Customize your laptop</h2>
-            {features}
+            <Features features={this.props.features} selected={this.state.selected} updateFeature={this.updateFeature}/>
           </form>
           <section className="main__summary">
             <h2>Your cart</h2>
--- src/components/SelectProcessor.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function SelectProcessor() {
-
-}
-
-export default SelectProcessor;
